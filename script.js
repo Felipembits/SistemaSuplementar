ac71c0f1873b4cbc72c969345e7b8f4d7445c733
Felipe Martins Bittencourt <felipebittencourt@id.uff.br>
2023-12-27 01:03:17 -0300

    style: melhorando responsividade

--- script.js
+++ script.js
@@ -19,20 +19,9 @@ class MobileNavBar {
     this.handleClick = this.handleClick.bind(this);
   }
 
-  animateLinks() {
-    this.navLinks.forEach((link) => {
-      link.style.animation
-      ? (link.style.animation = "")
-      : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`)
-    })
-  
-  }
-
   handleClick() {
     this.navList.classList.toggle(this.activeClass);
-    this.animateLinks()
   }
-
 
   addClickEvent() {
     this.mobileMenu.addEventListener('click', this.handleClick);
